496f3008b588c69e7f90b94243121c4fde12c22f
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-09-22 17:50:50 -0400

    Place simple get request inside get method

--- libhttpc.py
+++ libhttpc.py
@@ -7,13 +7,17 @@ import sys
 host = 'httpbin.org'
 port = 80
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-#request = 'GET /status/418 HTTP/1.0\r\nHost: httpbin.org\r\n\r\n'
-s.sendall('GET /status/418 HTTP/1.0\r\n'.encode("utf-8"))
-s.sendall(('Host: httpbin.org\r\n\r\n').encode("utf-8"))
-#response = s.recv(len(request), socket.MSG_WAITALL)
-response = s.recv(4096, socket.MSG_WAITALL)
-response2 = s.recv(2)
-sys.stdout.write( response.decode("utf-8"))
-
+def get(host):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host, 80))
+        #request = 'GET /status/418 HTTP/1.0\r\nHost: httpbin.org\r\n\r\n'
+        s.sendall('GET /status/418 HTTP/1.0\r\n'.encode("utf-8"))
+        s.sendall(('Host: httpbin.org\r\n\r\n').encode("utf-8"))
+        #response = s.recv(len(request), socket.MSG_WAITALL)
+        response = s.recv(4096, socket.MSG_WAITALL)
+        response2 = s.recv(2)
+        sys.stdout.write( response.decode("utf-8"))
+    finally:
+        s.close()
+#get(host)
