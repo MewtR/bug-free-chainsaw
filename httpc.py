c8a3c7322030d6922edd64ddde6d6d391e15cb5e
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-09-23 09:24:36 -0400

    Add all options needed for get to the client

--- httpc.py
+++ httpc.py
@@ -1,11 +1,47 @@
 import argparse
 import libhttpc
 
-parser = argparse.ArgumentParser(prog='httpc', usage='httpc command [arguments]', description='httpc is a curl like application but supports HTTP protocol only.')
-parser.add_argument('command', default='help', nargs='?', choices=['help', 'get', 'post'])
+def commandDescriptions():
+   return """
+The commands are: 
+   get           executes a HTTP GET request and prints the response. 
+   post          executes a HTTP POST request and prints the response. 
+   help          print this help screen.
+"""
+
+parser = argparse.ArgumentParser(prog='httpc', usage='httpc command [arguments]', description='httpc is a curl like application but supports HTTP protocol only.', formatter_class=argparse.RawTextHelpFormatter, epilog=commandDescriptions())
+#parser.add_argument('command', default='help', nargs='?', choices=['help', 'get', 'post'], help=argparse.SUPPRESS)
+subparsers = parser.add_subparsers(dest='command', help=argparse.SUPPRESS)
+
+# create the parser for the get command
+parser_get = subparsers.add_parser('get', prog='httpc get', add_help=False)
+parser_get.add_argument('-v','--verbose', action='store_true', help='Prints the detail of the response such as protocol, status, and headers.')
+parser_get.add_argument('-h',nargs=1, metavar='key:value',help='Associates headers to HTTP Request with the format \'key:value\'.')
+parser_get.add_argument('URL')
+
+# create the parser for the post command
+parser_post = subparsers.add_parser('post', prog='httpc post')
+parser_post.add_argument('--opt3', action='store_true')
+parser_post.add_argument('--opt4', action='store_true')
+
+# create the parser for the help command
+parser_help = subparsers.add_parser('help', help=argparse.SUPPRESS)
+parser_help.add_argument('method', choices=['get','post',''], default='', nargs='?')
+
+
 args = parser.parse_args()
+print (args)
 if args.command == 'help':
-    parser.print_help()
+    if args.method == 'get':
+        parser_get.print_help()
+    elif args.method == 'post':
+        parser_post.print_help()
+    else:
+        parser.print_help()
 elif args.command == 'get':
     print ("GET used")
+    #parser_get.print_help()
+elif args.command == 'post':
+    print ("POST used")
+    #parser_post.print_help()
 
--- libhttpc.py
+++ libhttpc.py
@@ -7,13 +7,13 @@ import sys
 host = 'httpbin.org'
 port = 80
 
-def get(host):
+def get(host, path='/'):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect((host, 80))
         #request = 'GET /status/418 HTTP/1.0\r\nHost: httpbin.org\r\n\r\n'
         s.sendall('GET /status/418 HTTP/1.0\r\n'.encode("utf-8"))
-        s.sendall(('Host: httpbin.org\r\n\r\n').encode("utf-8"))
+        s.sendall(('Host: '+host+'\r\n\r\n').encode("utf-8"))
         #response = s.recv(len(request), socket.MSG_WAITALL)
         response = s.recv(4096, socket.MSG_WAITALL)
         response2 = s.recv(2)
