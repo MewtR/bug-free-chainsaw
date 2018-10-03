0fdee42f650f39ad50c7dcd1131e424224b6482c
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-10-03 15:01:29 -0400

    Add content length to fix bug with post request.

--- libhttpc.py
+++ libhttpc.py
@@ -9,24 +9,25 @@ def makeRequest(host, path, verbose, headers=[], data=''):
         s.connect((host, 80))
         request = path +' HTTP/1.0\r\n'
         request += 'Host: '+ host +'\r\n'
+        request += 'Content-length: '+ str(len(data))+'\r\n'
         if headers:
             for h in headers:
-                #was list becuase of nargs in h option
+                #was list because of nargs in h option
                 request+= h+'\r\n'
         #End of request
         request+='\r\n'
         if 'POST' in request:
             if (data):
                 request+=data
-                request+='\r\n'
         s.sendall((request).encode("utf-8"))
         #response = s.recv(4096, socket.MSG_WAITALL)
         response = recv_all(s)
         response = response.decode("utf-8")
         (header, body) = response.split('\r\n\r\n')
+        #body = response.split('\r\n\r\n')
         if (verbose):
             sys.stdout.write(request)
-            sys.stdout.write(header)
+            #sys.stdout.write(header)
             sys.stdout.write('\r\n\r\n')
         sys.stdout.write(body)
     finally:
