8f5ac7ab5321695c726a7edb339dca9c9bd4d1ef
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-10-30 10:59:38 -0400

    Implement get requests (still need to do proper status codes for errors).

--- httpfs.py
+++ httpfs.py
@@ -6,10 +6,11 @@ import libhttpfs
 #parser = argparse.ArgumentParser(prog='httpfs', description='httpfs is a simple file server.', formatter_class=argparse.RawTextHelpFormatter)
 parser = argparse.ArgumentParser(prog='httpfs', description='httpfs is a simple file server.')
 parser.add_argument('-v','--verbose', action='store_true', help='Prints debugging messages.')
-parser.add_argument('-d', '--directory',type=str, default='/', help='Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.')
+parser.add_argument('-d', '--directory',type=str, default='./', help='Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.')
 parser.add_argument('-p', '--port', type=int, default=8080, help='Specifies the port number that the server will listen and serve at.')
 
 args = parser.parse_args()
 
+print (args)
 libhttpfs.run_server('127.0.0.1', args.port, args.directory)
 
--- libhttpfs.py
+++ libhttpfs.py
@@ -5,7 +5,7 @@ import re
 import threading
 import os
 
-def run_server(host, port, directory):
+def run_server(host, port, directory = './'):
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         listener.bind((host, port))
@@ -14,8 +14,7 @@ def run_server(host, port, directory):
         while True:
             conn, addr = listener.accept()
             #threading.Thread(target=handle_client, args=(conn, addr)).start()
-            handle_client(conn, addr)
-            print ("HELLOOOOOOOOO")
+            handle_client(conn, addr, directory)
     finally:
         listener.close()
         
@@ -25,7 +24,7 @@ def generate_headers(status):
     headers +='\r\n\r\n'
     return headers.encode("utf-8")
 
-def handle_client(conn, addr):
+def handle_client(conn, addr, directory):
     data = b''
     received_data = b''
     response = ''
@@ -40,7 +39,7 @@ def handle_client(conn, addr):
             #print (data.decode("utf-8"))
         if 'GET' in received_data.decode("utf-8"):
             received_data = received_data.decode("utf-8")
-            response = do_GET(received_data, conn, addr)
+            response = do_GET(received_data, conn, addr, directory)
         print (received_data)
             #conn.sendall(data)
         conn.sendall(generate_headers(str(200)))
@@ -50,27 +49,46 @@ def handle_client(conn, addr):
         print ("Closing connection with client")
         conn.close()
 
-def do_GET(received_data, conn, addr):
+def do_GET(received_data, conn, addr, directory):
     response = ''
     file_requested = received_data.split(' ')[1]
-    files_in_current_dir = os.listdir()
+    files_in_working_dir = os.listdir(directory)
+    print ("Files in working directory are: ")
+    print (files_in_working_dir)
+    #List files in current working directory
     if file_requested == '/':
-        for f in files_in_current_dir:
+        for f in files_in_working_dir:
             response+=f
             response+='\n'
         return response
+    #Read from a file in the current working directory
     else:
+        check_permissions = file_requested.split('/')
+        if len(check_permissions) > 2:
+            print (check_permissions)
+            response = 'You do not have access to this directory'
+            return response
         f = re.findall('/(.*)', file_requested)[0]
-        if (os.path.isfile(f)):
+        print ("file is : ")
+        print (f)
+        if (os.path.isfile(directory+'/'+f) and (f in files_in_working_dir)):
             try:
-                with open(f, 'r') as myfile:
+                with open(directory+'/'+f, 'r') as myfile:
                     response = myfile.read()
-            except:
+            except Exception as e:
                 response = 'Unable to read file'
+                response += '\n'
+                response += str(e)
+            return response
+        elif (os.path.isdir(directory+'/'+f)):
+            response = 'You do not have access to this directory'
             return response
         else:
-            requested_dir = os.listdir(f)
-            for f in requested_dir:
-                response+=f
-                response+='\n'
+            response = 'File does not exist'
             return response
+       # else:
+       #     requested_dir = os.listdir(directory+'/'+f)
+       #     for f in requested_dir:
+       #         response+=f
+       #         response+='\n'
+       #     return response
